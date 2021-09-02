6f470f217b3aa4ff37c590ac5d453a8427a8166d
kyonishikawa <kyo24111@gmail.com>
2021-09-03 03:06:53 +0900

    0903

--- oop/204.py
+++ oop/204.py
@@ -1,5 +1,20 @@
 def decimalToBinary(decNumber):
-    #ここから書きましょう
-    
-def rec(x):
-    return rec(x%2)
+    output = ""
+    rem = 0
+    while decNumber > 1:
+        rem = decNumber % 2
+        # print("rem:"+str(rem))
+        output += str(rem)
+        decNumber //= 2
+        # print("new_dec:"+str(decNumber))
+        # print("output:"+str(output))
+    output += "1"
+    # print("final_output:"+str(output))
+    new_output = list(output)
+    new_output.reverse()
+    str_output = "".join(new_output)
+    # print(type(str_output))
+    # print(str_output)
+    return new_output
+
+decimalToBinary(1)

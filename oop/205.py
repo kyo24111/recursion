00b448d13b07ac2e289f4dee3814e017936d5ca5
kyonishikawa <kyo24111@gmail.com>
2021-09-05 10:49:10 +0900

    edit

--- oop/205.py
+++ oop/205.py
@@ -2,9 +2,12 @@ def decimalToHexadecimal(decNumber):
     output = ""
     while decNumber >= 16:
         rem = decNumber % 16
+        rem = str(rem)
+        convert_16(rem)
         decNumber //= 16
         print("dec:" + str(decNumber))
-        output += str(rem)
+        output += rem
+    convert_16(decNumber)
     output += str(decNumber)
     print(output)
     output_tmp = list(output)
@@ -12,4 +15,19 @@ def decimalToHexadecimal(decNumber):
     final_output = "".join(output_tmp)
     return final_output
 
-decimalToHexadecimal(256)
+def convert_16(n):
+    if n == "10":
+        n = "A"
+    elif n == "11":
+        n = "B"
+    elif n == "12":
+        n = "C"
+    elif n == "13":
+        n = "D"
+    elif n == "14":
+        n = "E"
+    elif n == "15":
+        n = "F"
+    return n
+
+decimalToHexadecimal(255)
